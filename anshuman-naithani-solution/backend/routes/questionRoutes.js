a28adc680387d84403a94660aa2b2f3638361b2c
Anshuman Naithani <anshumannaithani7@gmail.com>
2025-06-17 22:03:17 +0530

    changes

--- anshuman-naithani-solution/backend/routes/questionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { generateQuestions } = require("../controllers/questionController");
-
-// POST route to generate technical questions
-router.post("/generate", generateQuestions);
-
-module.exports = router;
